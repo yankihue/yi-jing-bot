490dc200c46432567e1feecedce62053fd669e91
Yankı Sırtaş <yankisirtas@hotmail.com>
2023-04-10 13:27:06 +0300

    Add commands subdirectory

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const dotenv = require("dotenv");
+const fs = require("node:fs");
+const path = require("node:path");
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
 dotenv.config();
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -17,8 +37,9 @@ client.login(process.env.TOKEN);
 const hexagrams = require("./hexagrams");
 
 // * Configures options to pass for CLI
-
-generateHexagram();
+function returnThings() {
+  return generateHexagram();
+}
 
 const toss = () => {
   // * traditional calculations to generate i-ching from coin flips assign 'heads' the value of 3 and 'tails' the value of two.
